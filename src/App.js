f8f170e9db71abb35fe4852d77d7f6f50111e1ae
minhthao <minhthao5648@gmail.com>
2020-05-23 17:11:22 +0700

    Add find and list book

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import BookList from "./components/BookList";
+import axios from "axios";
+import { Container } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      value:"",
+      titile:[]
+    }
+  }
+  handleChange = (event)=>{
+    this.setState({value: event.target.value});
+  };
+  onSubmit = (event) =>{
+    event.preventDefault();
+    axios.get("http://localhost:4000/books/find?q="+ this.state.value)
+    .then (res => {
+      this.setState ({
+        titile:res.data
+      })
+    })
+  }
+
+  componentDidMount(){
+    axios.get("http://localhost:4000/books/api/books")
+    .then(res =>{
+      this.setState({
+        titile:res.data
+      })
+    })
+  }
+  render() {
+    return (
+      
+      <div className = "container">
+        {/*Find books */}
+        <div className = "form-find">
+          <form onSubmit = {this.onSubmit}>
+            <label> Find book  <br/>
+              <input 
+              type ="text" 
+              name ="q" 
+              value={this.state.value} 
+              onChange={this.handleChange} 
+              placeholder ="Type title here" />
+            </label>
+            <button type="submit">Find</button>
+          </form>
+        </div>
+        <div className = "container-list">
+          {/* list Books */}
+          {this.state.titile.map((tlbook)=>{
+            return <BookList 
+            tl={tlbook.tl}
+            description = {tlbook.description}
+            price ={tlbook.price}
+            image = {tlbook.image}
+            />
+          })}
+          </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
